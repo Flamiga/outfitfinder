97fb2d1a5a52538fca7fcf5692efe362c5e39545
Unknown <eaafla@students.eaaa.dk>
2019-02-18 13:25:46 +0100

    loadImages i js filen

--- js/main.js
+++ js/main.js
@@ -2,21 +2,32 @@
 
 // Get the rigth photo to swipe
 let knap = "";
+
 function continueF() {
-  reset();
-console.log(del);
-let imageURL = document.querySelector("#blah").src;
-showPage("outfitfinder");
+  loadImages();
+  console.log(del);
+  let imageURL = document.querySelector("#blah").src;
+  showPage("outfitfinder");
 
 
-if (del=="overdel"){
-document.querySelector("#nr1").src = imageURL;}
-else {
-document.querySelector("#nr2").src = imageURL;}
+  if (del == "overdel") {
+    document.querySelector("#ovedelcaroul").innerHTML = `
+        <div class="carousel-item">
+          <img src="${imageURL}" class="d-block w-100" alt="...">
+        </div>
+      `;
+  } else {
+    document.querySelector("#underdelcaroul").innerHTML = `
+        <div class="carousel-item">
+          <img src="${imageURL}" class="d-block w-100" alt="...">
+        </div>
+      `;
+    }
 }
 
+
 // reset
-function reset(){
+function reset() {
   document.getElementById('ovedelcaroul').innerHTML = `
   <div class="carousel-item active">
     <img id="nr1" src="img/top.png" class="d-block w-100" alt="...">
@@ -30,19 +41,19 @@ function reset(){
 
 
 // Go to the right page after choosing which style to upload + change of header
- let del = "";
+let del = "";
 
- function uploadOverdel(){
-   del= "overdel";
-   showPage("popover");
-   document.querySelector("#popoverheader").innerHTML="Upload overdel";
+function uploadOverdel() {
+  del = "overdel";
+  showPage("popover");
+  document.querySelector("#popoverheader").innerHTML = "Upload overdel";
 }
 
- function uploadUnderdel(){
-   del= "underdel";
-   showPage("popover");
-   document.querySelector("#popoverheader").innerHTML="Upload underdel";
- }
+function uploadUnderdel() {
+  del = "underdel";
+  showPage("popover");
+  document.querySelector("#popoverheader").innerHTML = "Upload underdel";
+}
 
 
 // hide all pages
@@ -76,16 +87,45 @@ setDefaultPage();
 // popover
 
 function readURL(input) {
-       if (input.files && input.files[0]) {
-           var reader = new FileReader();
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
 
-           reader.onload = function (e) {
-               $('#blah')
-                   .attr('src', e.target.result)
-                   .width(150)
-                   .height(200);
-           };
+    reader.onload = function(e) {
+      $('#blah')
+        .attr('src', e.target.result)
+        .width(150)
+        .height(200);
+    };
 
-           reader.readAsDataURL(input.files[0]);
-       }
-   }
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function loadImages(){
+
+fetch('json/data.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(json => {
+    console.log(json);
+    appendImages(json.overdel, "#ovedelcaroul");
+    appendImages(json.underdel, "#underdelcaroul");
+  });
+
+}
+
+loadImages();
+
+// Adds persons to the DOM by giving parameter, persons
+function appendImages(del, element) {
+  for (let image of del) { // looping trough all persons
+    console.log(image);
+    //creating person data, HTML tags and adding to the DOM, the element #gridPersons
+    document.querySelector(element).innerHTML += `
+        <div class="carousel-item">
+          <img src="${image}" class="d-block w-100" alt="...">
+        </div>
+      `;
+  }
+}
